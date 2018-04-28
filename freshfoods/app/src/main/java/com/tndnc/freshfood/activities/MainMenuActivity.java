c1f77724484759a1cb0b63da2640d20266fb3d40
leondoofus <teenage_96@yahoo.com>
2018-04-28 02:27:45 +0200

    call button added

--- freshfoods/app/src/main/java/com/tndnc/freshfood/activities/MainMenuActivity.java
+++ freshfoods/app/src/main/java/com/tndnc/freshfood/activities/MainMenuActivity.java
@@ -2,6 +2,7 @@ package com.tndnc.freshfood.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
@@ -24,5 +25,16 @@ public class MainMenuActivity extends AppCompatActivity {
     public void startAbout(View v) {
         Intent intent = new Intent(this, AboutActivity.class);
         startActivity(intent);
+    }
+
+    public void callWaiter(View v){
+        final Snackbar snackbar = Snackbar.make(findViewById(R.id.main_layout), "A waiter is successfully called", Snackbar.LENGTH_INDEFINITE);
+        snackbar.setAction("Ok", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                snackbar.dismiss();
+            }
+        });
+        snackbar.show();
     }
 }
